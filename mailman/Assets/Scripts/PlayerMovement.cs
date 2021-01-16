cc613ab0e21bb3d236269480009c1154010e1145
Uri Leon Freund <73687539+BalthazaarXIV@users.noreply.github.com>
2021-01-16 19:23:12 +0200

    Basic Project Setup
    Added URP, base assets and set up basic map

---
+++ mailman/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = 
+    }
+}
